7a46ea298be376064f883cf2830f2363e35fdced
Taisiya20 <58708234+Taisiya20@users.noreply.github.com>
2019-12-11 07:28:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'toolbar.php'; 
+
+?>
+<div style="margin-left: 5%">
+
+    <h2>Кинотеатр "Звезда"       вместимость зала: 240 мест</h2>
+	<img src='img/1k.jpg'>
+	<h2>Кинотеатр "Парк"         вместимость зала: 450 мест</h2>
+	<img src='img/4k.jpg'>
+	<h2>Кинотеатр "Люкс"         вместимость зала: 40 мест</h2>
+	<img src='img/2k.jpg'>
+	<h2>Кинотеатр "Hollywood"    вместимость зала: 308 мест</h2>
+	<img src='img/3k.jpg'>
+</div>
+<div style="margin-left: 25% ">
+    <img></div>
+</div>
+<div>
+    <!--Description-->
+
+</div>
+</body>
